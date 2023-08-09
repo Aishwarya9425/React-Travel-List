0f1a7e9e571805b331564dd22ab7e532f9ebb206
Aishwarya Janakiraman <aishwarya9425@gmail.com>
2023-08-09 22:17:46 +0530

    added states

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
+import { useState } from "react";
 const initialItems = [
   { id: 1, description: "Shorts", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 1, packed: false },
   { id: 2, description: "Charger", quantity: 1, packed: true },
-
 ];
 
 export default function App() {
@@ -19,10 +19,38 @@ function Logo() {
   return <h1>📝Plan to Travel🧳🏖️🏔️</h1>;
 }
 function Form() {
+  //react should control the form elements state not the dom
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+  function handleSubmit(e) {
+    //disable default html way of reloading the page when submitting
+    e.preventDefault();
+    if (!description) return;
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    setDescription("");
+    setQuantity("");
+  }
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip ? 🤔</h3>
-    </div>
+      <select value={quantity} onChange={(e) => setQuantity(e.target.value)}>
+        {Array.from({ length: 20 }, (_, index) => index + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Add your item here..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button> Add ➕</button>
+    </form>
   );
 }
 function PackingList() {
@@ -30,7 +58,7 @@ function PackingList() {
     <div className="list">
       <ul>
         {initialItems.map((item) => (
-          <Item item={item} />
+          <Item item={item} key={item.id} />
         ))}
       </ul>
     </div>
